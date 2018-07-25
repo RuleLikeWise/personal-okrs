2d0c176bb8402b08566a85db4dcf62ff79dd0f50
RuleLikeWise <songlian0111@foxmail.com>
2018-07-25 09:19:53 +0800

    Add front-end router

---
+++ src/components/Okrs.js
@@ -0,0 +1,48 @@
+import React from 'react';
+import Objective from './Objective';
+import 'bootstrap/dist/css/bootstrap-flex.min.css';
+import '../global.css';
+import logoUrl from '../parsys.svg';
+import Label from './Label';
+import { dataPreprocess } from '../modals/preprocess';
+import defaultOkrs from '../modals/okrs.json';
+
+const Okrs = ({ match }) => {
+  let okrs = null;
+  switch (match.params.quarter) {
+    case 'index':
+      okrs = defaultOkrs;
+      break;
+    default:
+      okrs = defaultOkrs;
+      break;
+  }
+
+  if (!okrs) {
+    return <div><h1>cannot find okrs</h1></div>;
+  }
+
+  const newData = dataPreprocess(okrs);
+  return <div className="container-fluid">
+    <div className="row">
+      <div className="inline col-xs">
+        <img src={ logoUrl } className="img-fluid logo" style={{ visibility: 'hidden' }}/>
+        <h1 className="title">ParSys OKR Dashboard
+          <Label type="info">{newData.score.toFixed(2)}</Label></h1>
+      </div>
+    </div>
+    <div className="row">
+      {newData.objectives.map((objective, key) => (
+          <Objective
+              key={key}
+              title={objective.name}
+              keyResults={objective.results}
+              goals={objective.okrs}
+              weight={objective.weight}
+              score={objective.score}/>
+      ))}
+    </div>
+  </div>;
+};
+
+export default Okrs;
--- src/index.js
+++ src/index.js
@@ -1,34 +1,13 @@
 import React from 'react';
 import { render } from 'react-dom';
-import Objective from './components/Objective';
-import data from './okrs.json';
-import 'bootstrap/dist/css/bootstrap-flex.min.css';
-import './global.css';
-import logoUrl from './parsys.svg';
-import Label from './components/Label';
-import { dataPreprocess } from './modals/preprocess';
-
-const newData = dataPreprocess(data);
+import { BrowserRouter as Router, Switch, Route, Redirect } from 'react-router-dom';
+import Okrs from './components/Okrs';
 
 render((
-  <div className="container-fluid">
-    <div className="row">
-      <div className="inline col-xs">
-        <img src={ logoUrl } className="img-fluid logo" style={ { visibility: 'hidden' } } />
-        <h1 className="title">ParSys OKR Dashboard
-          <Label type="info">{newData.score.toFixed(2)}</Label></h1>
-      </div>
-    </div>
-    <div className="row">
-      {newData.objectives.map((objective, key) => (
-        <Objective
-          key={key}
-          title={objective.name}
-          keyResults={objective.results}
-          goals={objective.okrs}
-          weight={objective.weight}
-          score={objective.score}/>
-      ))}
-    </div>
-  </div>
+    <Router>
+      <Switch>
+        <Redirect exact push from="/" to="/index"/>
+        <Route path="/:quarter" component={Okrs}/>
+      </Switch>
+    </Router>
 ), document.getElementById('root'));
